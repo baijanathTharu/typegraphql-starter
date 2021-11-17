354403417acf890229a8bbfbec363b89f427c876
baijanathTharu <bnchaudhary.tharu@gmail.com>
2021-11-17 23:15:49 +0545

    add custom validator to check unique emails

--- src/modules/user/inputs/register_input.ts
+++ src/modules/user/inputs/register_input.ts
@@ -1,5 +1,6 @@
 import { IsEmail, Length } from "class-validator";
 import { Field, InputType } from "type-graphql";
+import { IsEmailAlreadyExist } from "./is_email_already_exists";
 
 @InputType()
 export class RegisterInput {
@@ -13,6 +14,7 @@ export class RegisterInput {
 
   @Field()
   @IsEmail()
+  @IsEmailAlreadyExist({ message: "email already exists" })
   email: string;
 
   @Field()
