5d802c4caa94e4a229347c68e3ab3f41508ca001
baijanathTharu <bnchaudhary.tharu@gmail.com>
2021-11-18 19:50:30 +0545

    setup redis, session and cors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import Express from "express";
 import { buildSchema } from "type-graphql";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core/dist/plugin/landingPage/graphqlPlayground";
 import { createConnection } from "typeorm";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import Redis from "ioredis";
+import cors from "cors";
+
 import { UserResolver } from "./modules/user/resolvers";
 
 const main = async () => {
@@ -17,11 +22,43 @@ const main = async () => {
   const apolloServer = new ApolloServer({
     schema,
     plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+    context: ({ req }: any) => ({ req }),
   });
 
   await apolloServer.start();
 
   const app = Express();
+
+  /* *** cors *** */
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+  );
+  /* *** cors *** */
+
+  /* *** session middleware *** */
+  const RedisStore = connectRedis(session);
+  const redisClient = new Redis();
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redisClient,
+      }),
+      name: "qid",
+      secret: "put_in_env",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7years
+      },
+    })
+  );
+  /* *** session middleware *** */
 
   apolloServer.applyMiddleware({ app });
 
